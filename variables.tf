12a0146e08ba4565e730403d2a2980b8aeb64284
Aisosa Aimufua <aisosaj@gmail.com>
2023-09-23 01:34:37 +0000

    variables lab2p2

--- variables.tf
+++ variables.tf
@@ -3,13 +3,14 @@ variable "aws_region" {
   default = "us-east-1"
 }
 variable "vpc_name" {
-  type    = string
-  default = "demo_vpc"
+type = string
+default = "demo_vpc"
 }
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+type = string
+default = "10.0.0.0/16"
 }
+
 variable "private_subnets" {
   default = {
     "private_subnet_1" = 1
@@ -17,6 +18,19 @@ variable "private_subnets" {
     "private_subnet_3" = 3
   }
 }
+
+variable "public_subnets" {
+<<<<<<<<< saved version
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+=========
+  }
+}
+
 variable "public_subnets" {
   default = {
     "public_subnet_1" = 1
@@ -24,3 +38,4 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+>>>>>>>>> local version
