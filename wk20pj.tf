5a089780fed1cb4bd255a527f6d408b4956de80f
Aisosa Aimufua <aisosaj@gmail.com>
2023-10-02 14:28:46 +0000

    wk20pj jenkins-tf

--- wk20pj.tf
+++ wk20pj.tf
@@ -5,19 +5,17 @@ provider "aws" {
 
 # Launch EC2 Instance and Install Website
 resource "aws_instance" "jenkins_ec2_instance" {
-  ami                    = "ami-03a6eaae9938c858c"
-  instance_type          = "t2.micro"
-  subnet_id              = "subnet-01b339f7198110906"
-  vpc_security_group_ids = ["sg-0874bd3b99db42f68"]
-  key_name               = "tfkp"
-  user_data              = file("jenkins_install.sh")
+  ami             = "ami-03a6eaae9938c858c"
+  instance_type   = "t2.micro"
+  security_groups = ["jenkins-sg"]
+  key_name        = "tfkp"
+  user_data       = file("jenkins_install.sh")
 }
 
 # Security Group for Jenkins EC2 Instance
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins-sg"
   description = "Jenkins Security Group"
-  vpc_id      = "vpc-0cbebd2d690f60aa5"
 
   ingress {
     from_port   = 22
@@ -32,12 +30,22 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
-    egress {
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create private s3 bucket  
+resource "aws_s3_bucket" "jenkins_s3" {
+  bucket = "jenkins-redteamsos-s3"
+  acl    = "private"
+
+  tags = {
+    Name = "wk20tfs3"
   }
 }
 
